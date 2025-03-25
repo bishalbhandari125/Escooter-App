086d58fa177f957754e18679f7af3af39349e829
Bishal Bhandari <bhandaribishal6963@gmail.com>
2025-03-25 00:37:26 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Escooter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
